e1c5903acecefbce0befe7d32f5c15b21b1ca095
Quinn McHenry <quinn@jqm.us>
2020-02-28 14:16:44 -0500

    Initial Commit

---
+++ Contemplate/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Contemplate
+//
+//  Created by Quinn McHenry on 2/28/20.
+//  Copyright © 2020 Quinn McHenry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
